4c2f66674227ee2f60f11f210d5d2bef1a8e27c0
Zach Olivier <olivierzach618@gmail.com>
2020-05-20 22:22:23 -0700

    update data profile TODOs

--- data/profile_account_data.py
+++ data/profile_account_data.py
@@ -8,3 +8,4 @@ df = pd.read_csv(file_name, engine='python')
 # TODO: clean data - missing data, categorical data
 # TODO: find external keys
 # TODO: build key modeling features
+# TODO: think about time - when do we get this data?
