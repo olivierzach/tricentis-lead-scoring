0316783237426fa13ef5a2fb2c60addc6dfc65b2
Zach Olivier <olivierzach618@gmail.com>
2020-07-07 20:19:01 -0700

    update paths to training set for local testing

--- app/local_app_test.py
+++ app/local_app_test.py
@@ -8,13 +8,13 @@ First, setup the app to run locally on development Flask server:
     - from terminal within app directory
     - % export FLASK_APP=main.py
     - % flask run
+    - should see:  * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
     
 Next, send traffic to the app
-    - run this script
+    - run this script...
     - loads training data
-        - note: you will have to use the model/gbm_model.py script to pickle test data, too large for github
     - takes an instance of the training data, converts to json, sends it to the app
-    - app grabs the POST request, and returns the score in JSON format
+    - app grabs the POST request, scales the data, scores the data then returns the score in JSON format
 
 Example from first observation of training data:
 200 {"predict_prob": "0.00972129142988537", "predict_date": "2020-07-08 02:52:15.296447", "predict_flag": "0"}
@@ -22,8 +22,7 @@ Example from first observation of training data:
 """
 
 # grab the test data to test the API
-# gbm_model_x_test.sav is not included in this repo
-df_test = joblib.load('data/pickles/gbm_model_x_test.sav')
+df_test = joblib.load('app/model_assets/gbm_model_x_test_for_flask.sav')
 
 # put data into a json object to mirror incoming data from API
 df_test['json'] = df_test.apply(lambda x: x.to_json(), axis=1)
