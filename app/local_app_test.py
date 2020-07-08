bd14e571e6416d71a0e2fbc1d114babe2f42f4ce
Zach Olivier <olivierzach618@gmail.com>
2020-07-07 20:07:25 -0700

    notes on using x_test for local testing

--- app/local_app_test.py
+++ app/local_app_test.py
@@ -12,14 +12,17 @@ First, setup the app to run locally on development Flask server:
 Next, send traffic to the app
     - run this script
     - loads training data
+        - note: you will have to use the model/gbm_model.py script to pickle test data, too large for github
     - takes an instance of the training data, converts to json, sends it to the app
-    - app recieves the POST request, and returns the score in JSON format
+    - app grabs the POST request, and returns the score in JSON format
 
+Example from first observation of training data:
 200 {"predict_prob": "0.00972129142988537", "predict_date": "2020-07-08 02:52:15.296447", "predict_flag": "0"}
 
 """
 
 # grab the test data to test the API
+# gbm_model_x_test.sav is not included in this repo
 df_test = joblib.load('data/pickles/gbm_model_x_test.sav')
 
 # put data into a json object to mirror incoming data from API
