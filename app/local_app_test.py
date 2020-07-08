f73bdebb61596f203f2c058ed47588ba6f8de457
Zach Olivier <olivierzach618@gmail.com>
2020-07-07 20:00:00 -0700

    updated local test of model API

--- app/local_app_test.py
+++ app/local_app_test.py
@@ -1,24 +1,38 @@
 import requests
+from sklearn.externals import joblib
+
+"""
+Guide to run this flask app test locally...
+
+First, setup the app to run locally on development Flask server:
+    - from terminal within app directory
+    - % export FLASK_APP=main.py
+    - % flask run
+    
+Next, send traffic to the app
+    - run this script
+    - loads training data
+    - takes an instance of the training data, converts to json, sends it to the app
+    - app recieves the POST request, and returns the score in JSON format
+
+200 {"predict_prob": "0.00972129142988537", "predict_date": "2020-07-08 02:52:15.296447", "predict_flag": "0"}
+
+"""
+
+# grab the test data to test the API
+df_test = joblib.load('data/pickles/gbm_model_x_test.sav')
+
+# put data into a json object to mirror incoming data from API
+df_test['json'] = df_test.apply(lambda x: x.to_json(), axis=1)
 
 # point to local api host for testing
 api_url = 'http://127.0.0.1:5000/'
 
-# create a sample row data
-data = {
-    'email': 'example@domain.com',
-    'feature1': '0',
-    'feature2': '0',
-    'feature3': '0',
-    'feature4': '0',
-    'feature5': '0',
-    'feature6': '0',
-}
-
 # test send traffic to local server
-for i in range(200):
+data = df_test['json'][0]
 
-    # post request to the local api
-    r = requests.post(url=api_url, data=data)
+# post request to the local api
+r = requests.post(url=api_url, data=data)
 
-    # show what we get back
-    print(r.status_code, r.text)
+# show what we get back
+print(r.status_code, r.text)
